748c788f117f5e91f12180a6c633876ba4d51847
AppGara <70445309+appgara@users.noreply.github.com>
2021-05-22 04:40:04 +0700

    Update ERC20.sol

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -6,16 +6,12 @@ import "./IERC20.sol";
 import "./extensions/IERC20Metadata.sol";
 import "../../utils/Context.sol";
 
-/**
+/** --------------------------- www.AppGara.com -------------------------------
  * @dev Implementation of the {IERC20} interface.
  *
  * This implementation is agnostic to the way tokens are created. This means
  * that a supply mechanism has to be added in a derived contract using {_mint}.
  * For a generic mechanism see {ERC20PresetMinterPauser}.
- *
- * TIP: For a detailed writeup see our guide
- * https://forum.zeppelin.solutions/t/how-to-implement-erc20-supply-mechanisms/226[How
- * to implement supply mechanisms].
  *
  * We have followed general AppGara guidelines: functions revert instead
  * of returning `false` on failure. This behavior is nonetheless conventional
